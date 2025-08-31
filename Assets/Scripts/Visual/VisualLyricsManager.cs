2495ebc1733332c3e1a1de932e001804e45f6740
MINUTE <grynnano@gmail.com>
2025-08-31 21:50:41 +0900

    3D 모드 프로토타입

--- Assets/Scripts/Visual/VisualLyricsManager.cs
+++ Assets/Scripts/Visual/VisualLyricsManager.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using LunaLyrics.Data;
 using LunaLyrics.Util;
 using UnityEngine;
@@ -17,6 +19,12 @@ namespace LunaLyrics.Visual
         [Header("Position Setting")]
         public float minDistance = 150f;
         public float randomOffset = 300f;
+        public int maxUsedPosLength = 3;
+        public float zRange = 50;
+
+
+
+
 
         [Header("Jitter Setting")]
         public float update = 0.15f;
@@ -33,6 +41,8 @@ namespace LunaLyrics.Visual
         private PoolOption _poolOption;
 
         private Vector3 _lastTextPos;
+        private List<Vector3> _usedTextPos = new();
+
         private float _minX, _maxX, _minY, _maxY;
 
         private void Start()
@@ -40,18 +50,6 @@ namespace LunaLyrics.Visual
             _poolOption = new(textParent, "Lyrics", 5);
             _objectPool = new ObjectPool<VisualLyrics>(lyricsText, _poolOption, InitText, null);
             ResetLyrics();
-
-            // 현재 캔버스 크기 기반으로 범위 지정
-            float screenWidth = textParent.rect.width;
-            float screenHeight = textParent.rect.height;
-
-            float paddingX = screenWidth * 0.05f;
-            float paddingY = screenHeight * 0.1f;
-
-            _minX = textParent.rect.xMin + paddingX;
-            _maxX = textParent.rect.xMax - paddingX;
-            _minY = textParent.rect.yMin + paddingY;
-            _maxY = textParent.rect.yMax - paddingY;
 
             _lastTextPos = Vector3.zero;
 
@@ -76,22 +74,34 @@ namespace LunaLyrics.Visual
         private void ResetLyrics()
         {
             _lastTextPos = Vector2.zero;
+            _usedTextPos.Clear();
+
             colorMaterial.SetColor("_GlowColor", textColors[Random.Range(0, textColors.Length)]);
             _objectPool.Clear();
         }
 
-        private Vector2 UpdateTextPosition(float textWidth, float textHeight)
+        private Vector3 UpdateTextPosition(float textWidth, float textHeight)
         {
+            float screenHeight = 2.0f * 10 * Mathf.Tan(Camera.main.fieldOfView * 0.5f * Mathf.Deg2Rad);
+            float screenWidth = screenHeight * Camera.main.aspect;
+
+            var offset = Camera.main.transform.position;
+            Debug.Log($"{screenHeight} {screenWidth}");
+
+            float paddingX = screenWidth * 0.05f;
+            float paddingY = screenHeight * 0.1f;
+
+            var finalMinX = textParent.rect.xMin + paddingX;
+            var finalMaxX = Mathf.Max(finalMinX, textParent.rect.xMax - paddingX - textWidth);
+            var finalMinY = textParent.rect.yMin + paddingY + textHeight / 2;
+            var finalMaxY = textParent.rect.yMax - paddingY - textHeight / 2;
+
+            /*
             Vector3 direction = new Vector2((Random.value * (Random.value < 0.5f ? -1 : 1)) - 0.5f, (0.5f + Random.value * 2) * (Random.value < 0.5f ? -1 : 1)).normalized;
             Vector3 newPosition;
 
             // 기존 위치에서 무작위 방향 & 거리로 이동
             newPosition = _lastTextPos + direction * (minDistance + Random.Range(0, randomOffset));
-
-            var finalMinX = _minX;
-            var finalMaxX = Mathf.Max(_minX, _maxX - textWidth);
-            var finalMinY = _minY + textHeight / 2;
-            var finalMaxY = _maxY - textHeight / 2;
 
             // 화면 밖으로 나가면 반대쪽에서 나옴
             if (newPosition.x < finalMinX) newPosition.x = finalMaxX - newPosition.x;
@@ -104,7 +114,29 @@ namespace LunaLyrics.Visual
             newPosition.y = Mathf.Clamp(newPosition.y, finalMinY, finalMaxY);
 
             _lastTextPos = newPosition;
-            return newPosition;
+            */
+
+            Vector3 newPosition;
+            bool passed;
+            int count = 0;
+
+            do
+            {
+                newPosition = new(Random.Range(finalMinX, finalMaxX), Random.Range(finalMinY, finalMaxY));
+                passed = true;
+
+                foreach (var lastPos in _usedTextPos)
+                {
+                    if ((lastPos - newPosition).magnitude < minDistance) passed = false;
+                }
+            } while (!passed && count++ < 50);
+
+            _usedTextPos.Add(newPosition);
+            if (_usedTextPos.Count > maxUsedPosLength) _usedTextPos.RemoveAt(_usedTextPos.Count - 1);
+
+            newPosition.z = Random.Range(0, zRange);
+
+            return offset + newPosition;
         }
     }
 }
